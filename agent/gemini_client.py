0b01f930b1ae1c977d30009f60f98388adc831c7
Tahleel Shaikh <112774100+Tahleels@users.noreply.github.com>
2026-01-22 01:03:03 +0530

    Update gemini_client.py

--- agent/gemini_client.py
+++ agent/gemini_client.py
@@ -6,7 +6,7 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def call_gemini(prompt: str) -> str:
     response = client.models.generate_content(
-        model="gemini-1.5-flash",   # STABLE + AVAILABLE
+        model="gemini-2.5-flash",   # STABLE + AVAILABLE
         contents=prompt
     )
     return response.text
